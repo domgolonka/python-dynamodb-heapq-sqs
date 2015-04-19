438d6ef4ee047e49421cdf958f2f4a31edcc3122
taghavi <taghavi@sfu.ca>
2015-04-18 20:48:55 -0700

    modified DBops. fixed some issues in the fronend

---
+++ frontend/integration_test1.py
@@ -0,0 +1,24 @@
+# to use this script, do this in the terminal
+# (make sure simple.py is in current directory)
+# $ python simple.py
+
+import os, time
+
+'''
+def sleep(sec):
+	print "sleeping..."
+	for x in range(sec):
+		print "tick"
+		time.sleep(1)
+'''
+os.system("curl 'http://localhost:8080/retrieve?id=4'")
+os.system("curl 'http://localhost:8080/delete?id=4'")
+#os.system("curl 'http://localhost:8080/?id=4'")
+#os.system("curl 'http://localhost:8080/delete?id=4'")
+
+'''
+print "1: "
+os.system("curl 'http://localhost:8080/create?id=4&name=ali&activities=hi,bye'")
+sleep(5)
+os.system("curl 'http://localhost:8080/create?id=10&name=ethic&activities=go,me'")
+'''
